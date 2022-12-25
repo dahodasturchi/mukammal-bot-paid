fbe328fc65474f6561d18a2f18a7f5c4f7ea2adf
JAKHONGIR ISMOILOV <99004284+antaares@users.noreply.github.com>
2022-12-25 14:16:24 +0500

    Session changed to aiohttp.ClientSession
    Update, DeprecationWarning was prevented

--- utils/photograph.py
+++ utils/photograph.py
@@ -11,8 +11,9 @@ async def photo_link(photo: types.photo_size.PhotoSize) -> str:
             name='file',
             value=file,
         )
-        async with bot.session.post('https://telegra.ph/upload', data=form) as response:
-            img_src = await response.json()
+        async with aiohttp.ClientSession() as session:
+            async with session.post('https://telegra.ph/upload', data=form) as response:
+                img_src = await response.json()
 
     link = 'http://telegra.ph/' + img_src[0]["src"]
     return link
